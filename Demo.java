9ab321d5c3ed3f51295e86f954b2eae3f18871b7
avirup1994 <avirupde28@gmail.com>
2020-04-18 18:08:02 +0530

    second commit

--- Demo.java
+++ Demo.java
@@ -1,20 +1,31 @@
 class A{
 
-    void call(int arr[])
+    int call(int arr[],int start,int end,int n)
     {
-        int sum=0;
-        for(int i=0;i<arr.length;i++)
+        int mid=start+(end-start)/2;
+        if(start<=end)
         {
-            sum=sum+arr[i];
+            if(arr[mid]==n)
+            {
+                return mid;
+            }
+            else if(arr[mid]>n)
+            {
+                return call(arr, 0, mid-1, n);
+            }
+            else{
+                return call(arr, mid+1, end, n);
+            }
         }
-        System.out.print(sum);
-          
+        else{
+            return -1;
+        }
     }
-
     public static void main(String args[])
     {
-        int arr[]={-3,2,3};
+        int arr[]={2,4,5,7,8,12};
         A ob=new A();
-        ob.call(arr);
+        int res=ob.call(arr, 0, arr.length-1,8);
+        System.out.print(res);
     }
 }
